5b1fef4b918d39dc24b71663d60775b53831a442
Metehan <meteha.gunday@icloudcom>
2023-09-04 13:51:31 +0300

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Metehan Günday on 4.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
